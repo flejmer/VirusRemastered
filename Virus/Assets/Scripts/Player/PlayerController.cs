618343be5786ff5752a0fa1e3bd22c32414285a8
flejmer <jflejmer@gmail.com>
2015-04-13 15:48:11 +0200

    Added player controller
    Player controller script now allows basic movement and rotates game
    object to face mouse position.

---
+++ Virus/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float rotationSpeed = 10;
+
+    private Rigidbody rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MouseRotation();
+        Movement();
+    }
+
+    void MouseRotation()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        }
+    }
+
+    void Movement()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float hori = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(hori, 0, vert);
+        rbody.MovePosition(transform.position + movement * movementSpeed * Time.deltaTime);
+    }
+}
