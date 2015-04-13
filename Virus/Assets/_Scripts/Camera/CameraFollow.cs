eed5b0419604787c8b202b4b62e54f95e06f9188
flejmer <jflejmer@gmail.com>
2015-04-13 19:38:52 +0200

    Added camera following target script
    Script makes it possible for camera to follow appointed target with the
    option of smoothing.

---
+++ Virus/Assets/_Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetToFollow;
+    public bool cameraSmoothness = true;
+    public float smoothnessStrength = 20;
+
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        offset = transform.position - targetToFollow.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        if (targetToFollow)
+        {
+            Vector3 destination = targetToFollow.position + offset;
+
+            if (cameraSmoothness)
+            {
+                transform.position = Vector3.Lerp(transform.position, destination, smoothnessStrength * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = destination;
+            }
+        }
+        else
+        {
+            Debug.Log("No target to follow chosen");
+        }
+    }
+}
---
+++ Virus/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 15;
+    public float rotationSpeed = 10;
+
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MouseRotation();
+        Movement();
+    }
+
+    void MouseRotation()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+        
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            rbody.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void Movement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if(vertical != 0 || horizontal != 0)
+        {
+            Vector3 movement = new Vector3(horizontal, 0, vertical);
+            rbody.MovePosition(transform.position + movement * movementSpeed * Time.deltaTime);
+        }
+    }
+}
