8f955a6e0799428af721149f2467150f4f6a78e9
flejmer <jflejmer@gmail.com>
2015-04-16 19:38:04 +0200

    Added script controlling simple missile

---
+++ Virus/Assets/_Scripts/Projectiles/SimpleMissileController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMissileController : MonoBehaviour
+{
+    public float missileSpeed = 20;
+
+    private float rayLenght = 0.75f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        FireRay(true, transform.TransformDirection(Vector3.forward));
+        FireRay(true, transform.TransformDirection(Vector3.back));
+        FireRay(true, transform.TransformDirection(Vector3.left));
+        FireRay(true, transform.TransformDirection(Vector3.right));
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * missileSpeed * Time.deltaTime, this.transform);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    }
+
+    void FireRay(bool debug, Vector3 dir)
+    {
+        if (debug)
+            Debug.DrawRay(transform.position, dir * rayLenght, Color.red);
+
+        if (Physics.Raycast(transform.position, dir, rayLenght))
+            print("Ray hit something");
+    }
+}
