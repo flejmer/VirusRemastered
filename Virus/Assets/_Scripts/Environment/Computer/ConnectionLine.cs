1670c67b324b7ec5477b028ec7a169d7ff0aab37
flejmer <jflejmer@gmail.com>
2015-05-13 15:49:30 +0200

    Added connection line
    Connection indicator between player and computer.

---
+++ Virus/Assets/_Scripts/Environment/Computer/ConnectionLine.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionLine : MonoBehaviour
+{
+    public Transform Origin;
+    public Transform Destination;
+    public int SizeOfLineElementsPos = 5;
+
+    private LineRenderer _lineRenderer;
+    private int _lrSize;
+
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.SetVertexCount(SizeOfLineElementsPos);
+        _lrSize = SizeOfLineElementsPos;
+    }
+
+    void Update()
+    {
+        DrawLine();
+    }
+
+
+    void DrawLine()
+    {
+        if (SizeOfLineElementsPos != _lrSize)
+        {
+            _lineRenderer.SetVertexCount(SizeOfLineElementsPos);
+            _lrSize = SizeOfLineElementsPos;
+        }
+
+        if (Origin == null || Destination == null) return;
+
+        _lineRenderer.SetPosition(0, Origin.position);
+
+        var distance = Destination.position - Origin.position;
+        var dir = distance / (SizeOfLineElementsPos - 1);
+
+        for (var i = 1; i < SizeOfLineElementsPos; i++)
+        {
+            var tempTransform = new GameObject().transform;
+            tempTransform.position = new Vector3(Origin.position.x, Origin.position.y, Origin.position.z);
+            tempTransform.rotation = Quaternion.LookRotation(dir);
+            tempTransform.position += i * dir;
+
+            if (i < SizeOfLineElementsPos-1)
+            {
+                tempTransform.Translate(Vector3.left * Random.Range(-0.4f, 0.4f));
+            }
+
+            var pointpointAlongLine = new Vector3(tempTransform.position.x, tempTransform.position.y, tempTransform.position.z);
+            _lineRenderer.SetPosition(i, pointpointAlongLine);
+
+            Destroy(tempTransform.gameObject);
+        }
+    }
+}
---
+++ Virus/Assets/_Scripts/SomeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            DelegateTests.OnPlayerInRangeEvent(other.gameObject, gameObject);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            DelegateTests.OnPlayerOutOfRangeEvent(other.gameObject, gameObject);
+    }
+}
